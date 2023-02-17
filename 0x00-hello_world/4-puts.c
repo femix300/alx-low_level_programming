c88d94435453c319f20b9971054bb03e19d25c27
Peter Ajimoti <oluwaseyipeter450@gmail.com>
2023-02-17 03:36:54 -0800

    simple c program that prints text

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
-\* print text */
+/**
+ *main - a simple c program that displays text on the screen
+ *Return: 0 (success)
+*/ 
 int main(void)
 {
 	char text[] = "Programming is like building a multilingual puzzle";
