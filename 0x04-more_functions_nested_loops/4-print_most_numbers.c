884c2ea438a6b2a2b48381e03268afa4dcde5fae
Peter Ajimoti <oluwaseyipeter450@gmail.com>
2023-02-24 13:53:27 -0800

    print most numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include"main.h"
+/**
+ * print_most_numbers - print 0 - 9 asides
+ * 2 and 4
+ * Return: Always 0 (Success)
+*/
+void print_most_numbers(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	{
+		if (n == 2 || n == 4)
+			continue;
+		_putchar(num + 48);
+	}
+	_putchar('\n');
+}
