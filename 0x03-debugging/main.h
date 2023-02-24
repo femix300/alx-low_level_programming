527f66b9ef4e0670af177bb4bf13b767041b59bb
Peter Ajimoti <oluwaseyipeter450@gmail.com>
2023-02-24 12:36:36 -0800

    days of the year

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -14,6 +14,9 @@ void times_table(void);
 int add(int, int);
 void print_to_98(int n);
 void print_times_table(int n);
+
 int largest_number(int a, int b, int c);
+int convert_day(int month, int day);
+void print_remaining_days(int month, int day, int year);
 
 #endif
