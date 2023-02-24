791151b5654d2eac8c610f7c6087004b02b361e5
Peter Ajimoti <oluwaseyipeter450@gmail.com>
2023-02-24 13:57:17 -0800

    print most numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -8,11 +8,11 @@ void print_most_numbers(void)
 {
 	int n;
 
-	for (n = 0; n <= 9; n++)
+	for (n = 0; n <= 9; ++n)
 	{
 		if (n == 2 || n == 4)
 			continue;
-		_putchar(num + 48);
+		_putchar(n + 48);
 	}
 	_putchar('\n');
 }
