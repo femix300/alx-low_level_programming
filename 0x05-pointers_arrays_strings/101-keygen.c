15044eed3e2ce37bbb60e038347e0c9075a445fc
Peter Ajimoti <oluwaseyipeter450@gmail.com>
2023-02-27 16:30:38 -0800

    generates random valid passwords

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -3,30 +3,34 @@
 #include <time.h>
 
 /**
- * main - Entry point
+ * main - Generates a random password for 101-crackme
  *
  * Return: Always 0
  */
 int main(void)
 {
-	char password[7];
-	int i, ascii_value;
+	int i, sum, diff;
+	char password[100];
 
-	srand(time(0));
+	srand(time(NULL));
 
-	for (i = 0; i < 7; i++)
+	for (i = 0, sum = 0; sum < (2772 - 122); i++)
 	{
-		ascii_value = rand() % 94 + 33;
-
-		if (ascii_value == 32)
-			ascii_value++;
-
-		password[i] = (char)ascii_value;
+		password[i] = rand() % 94 + 32;
+		sum += password[i];
 	}
 
-	password[i] = '\0';
+	password[i] = 2772 - sum;
+	password[++i] = '\0';
 
-	printf("%s", password);
+	diff = 0;
+	for (i = 0; password[i]; i++)
+	{
+		diff += password[i];
+		printf("%c", password[i]);
+	}
+
+	printf("%d\n", diff);
 
 	return (0);
 }
