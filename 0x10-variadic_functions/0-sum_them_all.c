9cfe4b37668edeb4cfba3ec56a62e75fe387f157
Peter Ajimoti <oluwaseyipeter450@gmail.com>
2023-03-26 15:30:15 -0700

    added task 0

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,25 @@
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+int sum_them_all(const unsigned int n, ...)
+{
+	if (n == 0)
+		return (0);
+
+	va_list arg_p;
+
+	int i, sum;
+
+	sum = 0;
+
+	va_start(arg_p, n);
+
+	for (i = 0; i < n; i++)
+	{
+		sum += va_arg(arg_p, const unsigned int);
+	}
+
+	va_end(arg_p);
+
+	return (sum);
+}
---
+++ 0x10-variadic_functions/README.md
@@ -0,0 +1,3 @@
+0x10. C - Variadic functions
+
+This project is an intro to the use of Variadic functions in C
