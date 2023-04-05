03a5399d431beb79813f9a28a41fa005d5500920
femix300 <oluwaseyipeter450@gmail.com>
2023-04-04 22:43:05 -0700

    added task 4 and 5

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+* free_listint - frees a linked lists
+* @head: head pointer to the linked list
+*/
+
+void free_listint(listint_t *head)
+{
+	listint_t *temp, *temp2;
+
+	temp = NULL;
+
+	temp = head;
+
+	while (temp != NULL)
+	{
+		temp2 = temp;
+		free(temp2);
+
+		temp = temp->next;
+	}
+}
---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+* free_listint2 - frees a linked lists and sets the head to NULL
+* @head: double pointer to the head of the linked list
+*/
+
+void free_listint2(listint_t **head)
+{
+	listint_t *temp, *temp2;
+
+	temp = NULL;
+
+	temp = *head;
+
+	while (temp != NULL)
+	{
+		temp2 = temp;
+		free(temp2);
+
+		temp = temp->next;
+	}
+
+	*head = NULL;
+
+}
