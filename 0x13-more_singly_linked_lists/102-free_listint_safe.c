5e790362956979212301a117c9a24751e797071a
femix300 <oluwaseyipeter450@gmail.com>
2023-04-05 15:24:57 -0700

    added task 13

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,38 @@
+#include "lists.h"
+
+/**
+* free_listint_safe - frees a listint_t list
+* @h: pointer to the head of the listint_t list
+* Return: size of the list that was free'd
+*/
+
+size_t free_listint_safe(listint_t **h)
+{
+	listint_t *temp, *current;
+
+	size_t count;
+
+	count = 0;
+	temp = NULL;
+	current = *h;
+
+	while (current != NULL)
+	{
+		count++;
+		if (current < current->next)
+		{
+			temp = current;
+			current = current->next;
+			free(temp);
+		}
+		else
+		{
+			free(current);
+			break;
+		}
+	}
+
+	*h = NULL;
+
+	return (count);
+}
