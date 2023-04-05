2321954318502f57eab23d4374a88d95cb384d90
femix300 <oluwaseyipeter450@gmail.com>
2023-04-05 15:31:39 -0700

    edited task 13

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -16,7 +16,7 @@ size_t free_listint_safe(listint_t **h)
 	temp = NULL;
 	current = *h;
 
-	if (*head == NULL || head == NULL)
+	if (*h == NULL || h == NULL)
 		return (0);
 
 	while (current != NULL)
