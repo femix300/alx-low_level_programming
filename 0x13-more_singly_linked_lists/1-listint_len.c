435ef79289c6bef879bd33c161f489c5dfc8258d
femix300 <oluwaseyipeter450@gmail.com>
2023-04-04 21:43:14 -0700

    added task 1

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,1 +1,22 @@
+#include "lists.h"
 
+/**
+* listint_len - finds the length of a linked list
+* @h: head pointer to the linked list
+* Return: length of the linked list
+*/
+
+size_t listint_len(const listint_t *h)
+{
+	size_t count;
+
+	count = 0;
+
+	while (h != NULL)
+	{
+		count++;
+		h = h->next;
+	}
+
+	return (count);
+}
