1eaa84934d58faaa87e87c1f73478428c3c7c4bd
femix300 <oluwaseyipeter450@gmail.com>
2023-04-10 17:27:12 -0700

    added task 1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -9,22 +9,16 @@ void print_binary(unsigned long int n)
 	unsigned long int tmp;
 	int change;
 
-	tmp = n;
-	change = 0;
-
 	if (n == 0)
 	{
 		printf("0");
 		return;
 	}
 
-	while (tmp > 0)
-	{
-		tmp >>= 1;
-		change++;
-	}
+	for (tmp = n, change = 0; (temp >>= 1) > 0; change++)
+		;
 
-	for (change = sizeof(n) * 8 - 1; change >= 0; change--)
+	for (; change >= 0; change--)
 	{
 		if ((n >> change) & 1)
 		{
