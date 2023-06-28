9d9b778cfb8ac4ac719f277467802149c5f85f7a
Peter Ajimoti <oluwaseyipeter450@gmail.com>
2023-06-28 13:42:26 -0700

    added file 103

--- 0x17-doubly_linked_lists/103-keygen.c
+++ 0x17-doubly_linked_lists/103-keygen.c
@@ -1,47 +1,46 @@
+
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 
 /**
- * main - Generate a key depending on a username for crackme5.
- * @argc: Number of arguments passed.
- * @argv: Arguments passed to main.
+ * main - this will generate a key depending on a username for crackme5
+ * @argc: is the number of arguments passed
+ * @argv: arguments that are passed to main
  *
- * Return: 0 on success, 1 on error.
+ * Return: 0 on success, 1 on error
  */
 int main(int argc, char *argv[])
 {
+	unsigned int i, b;
+	size_t len, add;
 	char *l = "A-CHRDw87lNS0E9B2TibgpnMVys5XzvtOGJcYLU+4mjW6fxqZeF3Qa1rPhdKIouk";
 	char p[7] = "      ";
-	size_t len, i;
-	unsigned int hash;
 
 	if (argc != 2)
 	{
 		printf("Correct usage: ./keygen5 username\n");
 		return (1);
 	}
-
 	len = strlen(argv[1]);
-
-	hash = 0;
-	for (i = 0; i < len; i++)
-	{
-		hash += (unsigned int)argv[1][i];
-		hash *= (unsigned int)argv[1][i];
-	}
-
-	srand((unsigned int)argv[1][0] ^ 14);
-
 	p[0] = l[(len ^ 59) & 63];
-	p[1] = l[(hash ^ 79) & 63];
-	p[2] = l[(hash ^ 85) & 63];
+	for (i = 0, add = 0; i < len; i++)
+		add += argv[1][i];
+	p[1] = l[(add ^ 79) & 63];
+	for (i = 0, b = 1; i < len; i++)
+		b *= argv[1][i];
+	p[2] = l[(b ^ 85) & 63];
+	for (b = argv[1][0], i = 0; i < len; i++)
+		if ((char)b <= argv[1][i])
+			b = argv[1][i];
+	srand(b ^ 14);
 	p[3] = l[rand() & 63];
-	p[4] = l[(hash ^ 239) & 63];
-	p[5] = l[(rand() ^ 229) & 63];
-
+	for (b = 0, i = 0; i < len; i++)
+		b += argv[1][i] * argv[1][i];
+	p[4] = l[(b ^ 239) & 63];
+	for (b = 0, i = 0; (char)i < argv[1][0]; i++)
+		b = rand();
+	p[5] = l[(b ^ 229) & 63];
 	printf("%s\n", p);
-
 	return (0);
 }
-
