5b5a2d73de4d3753f501a683c69e52c4ea977922
Peter Ajimoti <oluwaseyipeter450@gmail.com>
2023-06-28 10:29:26 -0700

    broke task 7 into two in order to pass betty checks

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,63 +1,75 @@
-#include "lists.h"
 
 /**
- * insert_dnodeint_at_index - inserts a new node at a given index
- * in a linked list
- * @h: a pointer to the head pointer of the linked list
- * @idx: the given index where the node is to be inserted
- * @n: the data to be inserted in the node
- * Return: the address of the added node or Null if inserting the
- * node was not possible
+ * create_dnodeint - Creates a new doubly linked list node
+ * @n: The value to store in the new node
+ * @prev: Pointer to the previous node in the list
+ * @next: Pointer to the next node in the list
+ *
+ * Return: Pointer to the newly created node, or NULL on failure
  */
-dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+dlistint_t *create_dnodeint(int n, dlistint_t *prev, dlistint_t *next)
 {
-	unsigned int count;
-	dlistint_t *new_node, *temp;
+	dlistint_t *new_node;
 
-	count = 0;
 	new_node = (dlistint_t *)malloc(sizeof(dlistint_t));
 	if (!new_node)
 		return (NULL);
-	new_node->next = NULL;
-	new_node->prev = NULL;
-	new_node->n = n;
 
-	if (*h == NULL)
-	{
-		if (idx == 0)
-		{
-			*h = new_node;
-			return (new_node);
-		}
-		else
-		{
-			free(new_node);
-			return (NULL);
-		}
-	}
+	new_node->n = n;
+	new_node->prev = prev;
+	new_node->next = next;
+
+	return (new_node);
+}
+
+/**
+ * insert_dnodeint_at_index - Inserts a new node at a given position
+ * @h: Double pointer to the head of the doubly linked list
+ * @idx: Index of the list where the new node should be added
+ * @n: The value to store in the new node
+ *
+ * Return: Address of the new node, or NULL if it failed
+ */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *new_node, *temp;
+	unsigned int count;
+
+	if (!h)
+		return (NULL);
+
 	if (idx == 0)
 	{
-		new_node->next = *h;
-		(*h)->prev = new_node;
+		new_node = create_dnodeint(n, NULL, *h);
+		if (!new_node)
+			return (NULL);
+		if (*h)
+			(*h)->prev = new_node;
 		*h = new_node;
+
 		return (new_node);
 	}
+
+	if (!*h && idx > 0)
+		return (NULL);
 	temp = *h;
-	while (temp != NULL)
+	count = 0;
+
+	while (temp)
 	{
-		if (count == (idx - 1))
+		if (count == idx - 1)
 		{
-			new_node->prev = temp;
-			new_node->next = temp->next;
-			if (temp->next != NULL)
+			new_node = create_dnodeint(n, temp, temp->next);
+			if (!new_node)
+				return (NULL);
+			if (temp->next)
 				temp->next->prev = new_node;
 			temp->next = new_node;
 			return (new_node);
 		}
-		count++;
 		temp = temp->next;
+		count++;
 	}
-	free(new_node);
 	return (NULL);
 }
 
