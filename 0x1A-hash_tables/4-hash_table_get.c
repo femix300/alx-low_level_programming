b69633ed323cd4497ed881fa077fb22ff84df160
Peter Ajimoti <oluwaseyipeter450@gmail.com>
2023-07-11 17:27:03 -0700

    added checks for null values passed in task 4

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -12,6 +12,9 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	unsigned int index;
 	hash_node_t *temp;
 
+	if (ht == NULL || key == NULL || *key == '\0')
+		return (NULL);
+
 	index = key_index((const unsigned char *)key, ht->size);
 	temp = ht->array[index];
 
