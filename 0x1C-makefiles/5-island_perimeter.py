33bff6212affba4df3b9b3b3e189efdc3e33b773
Peter Ajimoti <oluwaseyipeter450@gmail.com>
2023-08-23 21:20:43 +0100

    added task 5

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Defines a function that mearsures the perimeter of an island"""
+
+
+def island_perimeter(grid):
+    """Returns the perimeter of an island
+
+    in the grid,
+    0 represents a water zone
+    1 represents a land zone
+
+    Args:
+        grid(list): a list of list of integers
+    Returns: The perimeter of the island represented by grid
+    """
+    width = len(grid[0])
+    height = len(grid)
+    edges = 0
+    size = 0
+
+    for i in range(height):
+        for j in range(width):
+            if grid[i][j] == 1:
+                size += 1
+                if (j > 0 and grid[i][j - 1] == 1):
+                    edges += 1
+                if (i > 0 and grid[i - 1][j] == 1):
+                    edges += 1
+    return size * 4 - edges * 2
